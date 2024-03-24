a9a4d6903267299f3a7ca16bd0646785c73ec5de
adinadhs <adinadh.somepalli@gmail.com>
2024-03-24 17:02:27 -0500

    episode-01

---
+++ App.js
@@ -0,0 +1,14 @@
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement("div", 
+    {id: "child"}, 
+    React.createElement("h1", {}, "I'm a h1 tag"))
+);
+
+
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent); 
